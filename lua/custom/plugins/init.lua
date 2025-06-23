799e0fd31804db55e66a33aafbf522c7dc3ad9f8
TomTomXD <tomasvlcek.pce@gmail.com>
2025-06-24 00:18:27 +0200

    vim-doge plugin for generating documentation, enable import of custom plugins, turn of default mapping, remap doge-generate

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    -- Vim-doge plugin for generating documentation
+    'kkoomen/vim-doge',
+    run = ':call doge#install()',
+
+    -- All mappings are turned off
+    init = function()
+      vim.g.doge_enable_mappings = 0
+    end,
+
+    -- Generating of documentation is mappped to <leader>d
+    vim.keymap.set('n', '<leader>d', '<Plug>(doge-generate)', { desc = 'Generate [D]ocumentation' }),
+  },
+}
